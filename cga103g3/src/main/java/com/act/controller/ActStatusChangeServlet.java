5b7acdf033241bc2aa379f15ba6f12ac6c2a238d
liwu3479 <111109037+liwu3479@users.noreply.github.com>
2022-09-14 19:22:28 +0800

    改註解

--- cga103g3/src/main/java/com/act/controller/ActStatusChangeServlet.java
+++ cga103g3/src/main/java/com/act/controller/ActStatusChangeServlet.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.act.model.ActService;
 import com.act.model.ActVO;
 
-@WebServlet("/ActStatusChangeServlet")
+
 public class ActStatusChangeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        Timer timer;
